ebf4c52ba4e9af1a96f06d5ae197917a86193db6
Syahril <everaldo@umich.edu>
2014-06-10 22:46:47 -0400

    Added user info

---
+++ public/javascripts/global.js
@@ -0,0 +1,63 @@
+// Userlist data array for filling in info box
+var userListData = [];
+
+// DOM Ready =============================================================
+$(document).ready(function() {
+
+    // Populate the user table on initial page load
+    populateTable();
+	
+	//Username link click
+	$('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+
+});
+
+// Functions =============================================================
+
+// Fill table with data
+function populateTable() {
+
+    // Empty content string
+    var tableContent = '';
+
+    // jQuery AJAX call for JSON
+    $.getJSON( '/users/userlist', function( data ) {
+		
+		 userListData = data;
+
+        // For each item in our JSON, add a table row and cells to the content string
+        $.each(data, function(){
+            tableContent += '<tr>';
+            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '" title="Show Details">' + this.username + '</a></td>';
+            tableContent += '<td>' + this.email + '</td>';
+            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+            tableContent += '</tr>';
+        });
+
+        // Inject the whole content string into our existing HTML table
+        $('#userList table tbody').html(tableContent);
+    });
+};
+
+// Show user info
+function showUserInfo(event) {
+	
+	//Prevent link from defaulting
+	event.preventDefault();
+	
+	//Retrieve username from link rel attribute
+	var thisUserName = $(this).attr('rel');
+	
+	//Get index of object based on id value
+	var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+	
+	//Get our user object
+	var thisUserObject = userListData[arrayPosition];
+	
+	//Populate info box
+	$('#userInfoName').text(thisUserObject.fullname);
+	$('#userInfoAge)').text(thisUserObject.age);
+	$('#userInfoGender').text(thisUserObject.gender);
+	$('#userInfoLocation').text(thisUserObject.location);
+	
+};
